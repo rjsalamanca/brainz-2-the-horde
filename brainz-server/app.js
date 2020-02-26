7d971b62f5f63eec0e6b2350d0e1b67c37390202
RJ Salamanca <salamancaronito@gmail.com>
2020-02-26 14:04:45 -0500

    removed a file utilizing dotevn module

--- brainz-server/app.js
+++ brainz-server/app.js
@@ -1,19 +1,21 @@
 const express = require('express'),
-    path = require('path'),
-    cookieParser = require('cookie-parser'),
-    logger = require('morgan'),
-    cors = require('cors');
+   path = require('path'),
+   cookieParser = require('cookie-parser'),
+   logger = require('morgan'),
+   cors = require('cors');
+
+require('dotenv').config();
 
 const indexRouter = require('./routes/index'),
-    usersRouter = require('./routes/users');
+   usersRouter = require('./routes/users');
 
 const corsOptions = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204,
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+   "origin": "*",
+   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+   "preflightContinue": false,
+   "optionsSuccessStatus": 204,
+   "Access-Control-Allow-Origin": "*",
+   "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
 };
 
 const app = express();
@@ -28,19 +30,19 @@ app.use(cors(corsOptions));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.send('error');
+   // render the error page
+   res.status(err.status || 500);
+   res.send('error');
 });
 
 
