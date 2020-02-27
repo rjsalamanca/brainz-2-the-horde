57e18fab8b43245a76d468fbee3e70d6ea6fcb72
RJ Salamanca <salamancaronito@gmail.com>
2020-02-27 14:23:51 -0500

    have to pre build since ec2 free limit cant build our app

---
+++ brainz-client/prebuild/precache-manifest.908a23a1f20ca9ab187895200b7f1497.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85b6b60f7437c2f7367d73da1576e2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "263943e8b55c8e9e827a",
+    "url": "/static/css/main.0369195b.chunk.css"
+  },
+  {
+    "revision": "778ca2990b827a0b53bf",
+    "url": "/static/js/2.28945a03.chunk.js"
+  },
+  {
+    "revision": "b505307cfa586854566d5c37cf4b919d",
+    "url": "/static/js/2.28945a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8181d5f458abd3513d2e",
+    "url": "/static/js/3.090393be.chunk.js"
+  },
+  {
+    "revision": "92382984969cadc4da0ef3708bdf689c",
+    "url": "/static/js/3.090393be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca4470e3c01e89b05c8",
+    "url": "/static/js/4.4304ad53.chunk.js"
+  },
+  {
+    "revision": "91d42836451b717a2853",
+    "url": "/static/js/5.e7c8301d.chunk.js"
+  },
+  {
+    "revision": "d74069c52b0a560654d7acf5aae4dab5",
+    "url": "/static/js/5.e7c8301d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9382b2b17e8c1d44d30",
+    "url": "/static/js/6.ddeb5cfd.chunk.js"
+  },
+  {
+    "revision": "e1dffb7f78bfcb321c60",
+    "url": "/static/js/7.c6a8366a.chunk.js"
+  },
+  {
+    "revision": "fa8c717b87e91656d08d",
+    "url": "/static/js/8.e29fc63b.chunk.js"
+  },
+  {
+    "revision": "d74069c52b0a560654d7acf5aae4dab5",
+    "url": "/static/js/8.e29fc63b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263943e8b55c8e9e827a",
+    "url": "/static/js/main.9965b215.chunk.js"
+  },
+  {
+    "revision": "81635fd4c3d24eb1c0e7",
+    "url": "/static/js/runtime-main.79c724e1.js"
+  },
+  {
+    "revision": "b10849c899982654fae3f01d02269043",
+    "url": "/static/media/Emulogic-zrEw.b10849c8.ttf"
+  },
+  {
+    "revision": "e35b0e8575b631cd27f3fa19b8fd1755",
+    "url": "/static/media/aim1.e35b0e85.mp4"
+  },
+  {
+    "revision": "ee6fdbed8057a8e028bd44b8a1b790a3",
+    "url": "/static/media/aim2.ee6fdbed.mp4"
+  },
+  {
+    "revision": "59feeb2ba810e467a6d69b407666f855",
+    "url": "/static/media/arcadev2.59feeb2b.png"
+  },
+  {
+    "revision": "ff0ccd1591dd1b63966214e52a0cba53",
+    "url": "/static/media/background1.ff0ccd15.png"
+  },
+  {
+    "revision": "1b997132edcfc4051c5879d971fe164f",
+    "url": "/static/media/backgroundv2.1b997132.jpg"
+  },
+  {
+    "revision": "e05763dfc6afd0858f70ae3fcdcc0b75",
+    "url": "/static/media/brainz-logo-1.e05763df.png"
+  },
+  {
+    "revision": "f8a365b325e9f8abc04f44a783476efd",
+    "url": "/static/media/computer-mouse.f8a365b3.gif"
+  },
+  {
+    "revision": "54630d45480f606b81344ad58596794f",
+    "url": "/static/media/gameover-once.54630d45.gif"
+  },
+  {
+    "revision": "0ab4e64e08a8120cdf2a1d28afb2ac0e",
+    "url": "/static/media/homepage.0ab4e64e.gif"
+  },
+  {
+    "revision": "3f8c3841c0203a21d1279f3a59d2b6ab",
+    "url": "/static/media/mouse-click.3f8c3841.gif"
+  },
+  {
+    "revision": "a4facf1a098be0c1abce04c9ec57857c",
+    "url": "/static/media/move1.a4facf1a.mp4"
+  },
+  {
+    "revision": "0486c6f2959e2c9567c366dde47b8f9b",
+    "url": "/static/media/move2.0486c6f2.mp4"
+  },
+  {
+    "revision": "eab43bdf6b283cae433cd381ed8688e4",
+    "url": "/static/media/shoot1.eab43bdf.mp4"
+  },
+  {
+    "revision": "6626abd8ad0065ef7fd6b21c920bda8b",
+    "url": "/static/media/shoot2.6626abd8.mp4"
+  },
+  {
+    "revision": "914287974e5d46ce9dce3e4fa77f8223",
+    "url": "/static/media/start.91428797.gif"
+  },
+  {
+    "revision": "cc3372003ffb05149170c5cc89f21e1f",
+    "url": "/static/media/wasd-fixed.cc337200.gif"
+  }
+]);
